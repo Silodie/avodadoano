bb5506208a28305cecc174944db549882998b585
Silodie <silvia.lopez.dieguez@gmail.com>
2026-01-10 16:41:12 +0100

    correccións en index

--- index.php
+++ index.php
@@ -17,10 +17,11 @@ header("Expires: 0");
                                 <img class="ind-main-img" src="assets/img/Fotos/index_Coimbra.jpg" alt="Nós">
                         </div>
                         <div class="col-12 col-lg-6 ind-main-text">
-                            <span>Despois de tantas aventuras xuntos, risas, viaxes e momentos compartidos, decidimos dar o seguinte paso e celebrar o noso amor rodeados das persoas máis importantes para nós.</span><br>
-                            <span>Marca este día no teu calendario: O <b>10</b> de <b>Outubro</b> do <b>2026</b> dirémonos ¡<a class='ind-main-hlink' href="gsheetAsistir.php">si, quero</a>!</span><br>
+                            <span>Despois de tantas aventuras xuntos, risas, viaxes e momentos compartidos, decidimos dar o seguinte paso e 
+                                festexar o noso amor coas persoas máis importantes para nós.</span><br>
+                            <span>Marca este día no teu almanaque: O <b>10</b> de <b>outubro</b> do <b>2026</b> dirémonos ¡<a class='ind-main-hlink' href="gsheetAsistir.php">si, quero</a>!</span><br>
                             <span>Queremos que sexa unha festa chea de ledicia, boa música, sorrisos e, sobre todo, moito agarimo.</span><br>
-                            <span><a class='ind-main-hlink' href="lugar.php">Aquí</a> atoparás toda a información sobre a voda: horarios, cómo chegar, recomendacións de aloxamento e algunha sorpresa máis.</span><br>
+                            <span><a class='ind-main-hlink' href="lugar.php">Aquí</a> atoparás toda a información da nosa voda: horarios, cómo chegar, recomendacións de aloxamento e algunha sorpresa máis.</span><br>
                             <span>Prepárate para un día máxico que quedará na nosa memoria para sempre.</span>
                         </div>
                     </div>
