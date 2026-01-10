a7bbef00c7e5219d08437e35136dc4e21586a219
Silodie <silvia.lopez.dieguez@gmail.com>
2026-01-10 17:18:53 +0100

    info hoteis

--- info.php
+++ info.php
@@ -12,25 +12,52 @@ include('lib/funciones.php');
                     <div class="row info-main ">
                         <div class="col-7">
                             <span class='info-main-textdes-data'>Aloxamentos</span><br>
-                            <!--<p class="lug-main-textdes"> Estamos á procura de espazo para todos en diferentes hoteis para facilitarvos o traballo o máximo posíbel.
-                                Debido á alta ocupación para as datas (celébrase un congreso de médicos neses días), si xa tedes clara a asistencia aconsellamos que vaiades buscando e reservando
-                                pola vosa conta nas diferentes plataformas dispoñíbeis (booking, airbnb, etc.)</p>
-                                <p class="lug-main-textdes">En canto teñamos máis información actualizaremos a web.</p>-->
                                <table class='info-main-table'>
                                     <tr class='info-main-textdes'>
                                         <th>Hotel</th>
-                                        <th>Teléfono</th>
                                         <th>Dirección</th>
+                                        <th>Teléfono</th>
+                                        <th>Email</th>
+                                        <th>Prezo</th>
+                                        <th>Cómo reservar</th>
                                         <th>Comentarios</th>
                                     </tr>
                                     <tr class='info-main-textau'>
                                         <td><a class='info-main-textau' target='_blank' href='https://www.carrishoteles.com/es/hotel-carris-casa-de-la-troya/'>Carrís Casa de la Troya</a></td>
+                                        <td><a class='info-main-textau' href='google.com/maps/place/Hotel+Carrís+Casa+de+la+Troya/@42.872253,-8.5526118,800m/data=!3m1!1e3!4m9!3m8!1s0xd2efe446a1a748f:0xe67f6d28b2b91642!5m2!4m1!1i2!8m2!3d42.881273!4d-8.5434741!16s%2Fg%2F1tmg7v5k?entry=ttu&g_ep=EgoyMDI2MDEwNy4wIKXMDSoASAFQAw%3D%3D' targe='_blank'> Rúa da Acivechería 14</a></td>
                                         <td>981555879</td>
-                                        <td>Rúa da Acivechería 14</td>
-                                        <td >Fan un 20% de desconto sobre a tarifa vixente no momento. 
-                                            Será preciso que reservedes por teléfono e digades que sodes invitados a voda de Pablo e Silvia.
-                                            Teñen poucas habitacións disponibles e non as bloquean</td>
-                                </tr>
+                                        <td>casadelatroya@carrishoteles.com</td>
+                                        <td>20% de desconto sobre a tarifa vixente no momento</td>
+                                        <td>Por teléfono indicando que sodes invitados á voda de Pablo e Silvia</td>
+                                        <td>Teñen poucas habitacións dispoñíbeis</td>
+                                    </tr>
+                                    <tr class='info-main-textau'>
+                                        <td><a class='info-main-textau' target='_blank' href='https://hostalmexico.com/'>Hotel México</a></td>
+                                        <td><a class='info-main-textau' href='https://www.google.com/maps/place/M%C3%A9xico+PR/@42.872253,-8.5526118,800m/data=!3m1!1e3!4m20!1m10!3m9!1s0xd2efe3e2b7c434f:0x225f8047074303db!2sM%C3%A9xico+PR!5m2!4m1!1i2!8m2!3d42.87215!4d-8.5498!16s%2Fg%2F1thsbn2w!3m8!1s0xd2efe3e2b7c434f:0x225f8047074303db!5m2!4m1!1i2!8m2!3d42.87215!4d-8.5498!16s%2Fg%2F1thsbn2w?entry=ttu&g_ep=EgoyMDI2MDEwNy4wIKXMDSoASAFQAw%3D%3D' targe='_blank'>Rúa República Arxentina, 33</a></td>
+                                        <td>981598000</td>
+                                        <td>info@hostalmexico.com</td>
+                                        <td>Hab. Individual: 40€/noite <br> Hab. Dobles: 75€/noite <br>Almorzo (opcional): +6.50€</td>
+                                        <td>Por teléfono/email indicando que é para o enlace de Silvia e Pablo</td>
+                                        <td>Piden número de tarxeta para a confirmación.<br>Cancelación gratuita ata o día antes.</td>
+                                    </tr>
+                                    <tr class='info-main-textau'>
+                                        <td><a class='info-main-textau' target='_blank' href='https://www.costavella.com/'>Hotel Costa Vella</a></td>
+                                        <td><a class='info-main-textau' href='https://www.google.com/maps/place/Hotel+Costa+Vella/@42.8831088,-8.5459406,615m/data=!3m2!1e3!4b1!4m10!3m9!1s0xd2efe44ecbb4e4b:0x8fd0eb6f3ad2099f!5m3!1s2026-01-12!4m1!1i2!8m2!3d42.8831049!4d-8.5433657!16s%2Fg%2F1tfcpmm4?entry=ttu&g_ep=EgoyMDI2MDEwNy4wIKXMDSoASAFQAw%3D%3D' targe='_blank'>Rúa da Porta da Pena, 17</a></td>
+                                        <td> - </td>
+                                        <td> - </td>
+                                        <td>Hab. Dobles: 135€/noite</td>
+                                        <td>Falade con nós para xestionalo</td>
+                                        <td>Teñen poucas habitacións dispoñíbeis</td>
+                                    </tr>
+                                    <tr class='info-main-textau'>
+                                        <td><a class='info-main-textau' target='_blank' href='https://www.rosarosae.es/'>Hotel Rosa Rosae</a></td>
+                                        <td><a class='info-main-textau' href='https://www.google.com/maps/place/Rosa+Rosae+(Santiago+de+Compostela)/@42.872552,-8.5491085,615m/data=!3m1!1e3!4m10!3m9!1s0xd2efe3f1d787c8d:0x5c095486ec39eb5e!5m3!1s2026-01-12!4m1!1i2!8m2!3d42.8725481!4d-8.5465336!16s%2Fg%2F1tg6rdk0?entry=ttu&g_ep=EgoyMDI2MDEwNy4wIKXMDSoASAFQAw%3D%3D' targe='_blank'>Rúa da Rosa, 7</a></td>
+                                        <td>981528300</td>
+                                        <td> - </td>
+                                        <td>Hab. individual: 100€/noite (almorzo incluido) <br> Hab. Dobles: 150€/noite (almorzo incluido)</td>
+                                        <td>Por teléfono indicando que é para o enlace de Silvia e Pablo</td>
+                                        <td>-</td>
+                                    </tr>
                                 </table>
                                 <p class="lug-main-textdes">Debido a alta ocupación para as datas, recomendamos valorar tamén a opción de airbnb. Para os que xa tedes claro que vides, recomendamos facer a reserva o antes posible.</p>
                                 <p class="lug-main-textdes"> Para os que vides en avión, o autobús <b>6A</b> sale cada media hora, custa sobre 1€ e vos deixa na praza de Galicia ou na estación de tren.</p>
--- lib/funciones.php
+++ lib/funciones.php
@@ -12,7 +12,7 @@
                 <head>
                     <meta charset='utf-8'>
                     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+                    <meta http-equiv='X-UA-Compatible' content='ie=edge,chrome=1'>
                     <link rel='icon' href='\assets\img\Logo\logo.png' type='img/png'/>
                     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
                     integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
