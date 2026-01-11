4fff7b84133924dd55386b1a493c306af2020512
Silodie <silvia.lopez.dieguez@gmail.com>
2026-01-11 21:50:10 +0100

    se añade hora

--- index_cast.php
+++ index_cast.php
@@ -22,7 +22,7 @@ header("Expires: 0");
                         <div class="col-12 col-lg-6 ind-main-text">
                             <span>Después de tantas aventuras juntos, risas, viajes y momentos compartidos, decidimos dar el siguiente paso 
                                 y festejar nuestro amor con las personas más importantes para nosotros.</span><br>
-                            <span>Marca este día en tu calendario: el <b>10</b> de <b>octubre</b> de <b>2026</b> nos diremos ¡<a class='ind-main-hlink' href="confirmación.php">sí, quiero</a>!</span><br>
+                            <span>Marca este día en tu calendario: el <b>10</b> de <b>octubre</b> de <b>2026</b>  a las <b>13.30h</b> nos diremos ¡<a class='ind-main-hlink' href="confirmación.php">sí, quiero</a>!</span><br>
                             <span>Queremos que sea una fiesta llena de alegría, buena música, sonrisas y, sobre todo,mucho cariño.</span><br>
                             <span><a class='ind-main-hlink' href="ubicación.php">Aquí</a> encontrarás toda la información de nuestra voda: horarios, cómo llegar, recomendaciones de alojamiento y alguna sorpresa más.</span><br>
                             <span>Prepárate para un día mágico que quedará en nuestra memoria para siempre.</span>
